a3d239bcf427add7b3538b4cf896d7b6c87c8888
oojahooo <oojahooo@gmail.com>
2022-01-28 08:14:04 +0900

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "Preface: 서론",
+        "title": "Preface: 서문",
         "excerpt":"첫 시작 본격적으로 컨퍼런스를 목표로 하는 연구를 진행하게 되며, 직접적으로 사용되는 이론과 기술을 찾아보고 적용할 일이 많아졌다. 매번 새롭게 닥치는 문제들을 해결해 나가는 묘미란 대학원생, 연구자들의 가장 흔하고 즐거운 일이라 생각한다. 그러나, 당장의 일을 해결하고 성과를 내기에 급급하다 보면 내가 정말 공부하고 싶었던 것들을 소홀히 하게 된다. 프로그래밍 언어론을 기반으로...","categories": ["Types and Programming Languages"],
         "tags": [],
         "url": "/types%20and%20programming%20languages/Ch1-Preface/",
