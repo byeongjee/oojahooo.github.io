44da7d1a73ed84f07773f9a373b4c2120167e2db
oojahooo <oojahooo@gmail.com>
2022-02-11 13:52:24 +0900

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,12 @@ var store = [{
         "title": "Preface: 서문",
         "excerpt":"첫 시작 본격적으로 컨퍼런스를 목표로 하는 연구를 진행하게 되며, 직접적으로 사용되는 이론과 기술을 찾아보고 적용할 일이 많아졌다. 매번 새롭게 닥치는 문제들을 해결해 나가는 묘미란 대학원생, 연구자들의 가장 흔하고 즐거운 일이라 생각한다. 그러나, 당장의 일을 해결하고 성과를 내기에 급급하다 보면 내가 정말 공부하고 싶었던 것들을 소홀히 하게 된다. 프로그래밍 언어론을 기반으로...","categories": ["Types and Programming Languages"],
         "tags": [],
-        "url": "/types%20and%20programming%20languages/Ch1-Preface/",
+        "url": "/types%20and%20programming%20languages/Ch0-Preface/",
+        "teaser": null
+      },{
+        "title": "Untyped Arithmetic Expressions: 타입을 따지지 않는 언어 설계 - 수식",
+        "excerpt":"Type System in Programming Languages 타입은 프로그래밍 언어를 배워본 사람이라면 한 번은 들어봤을 개념이다. 주로 번역될 때 자료형이라는 용어를 쓰는데, ‘data type’을 ‘자료의 형태’라 번역한 것이라 보면 된다. 컴퓨터 프로그래밍의 범주에서 type은 보통의 경우 data type을 의미하기 때문에 틀린 번역은 아니지만, 표현의 명료함과 일반성을 위해 앞으로는 타입이라고 명명하겠다. 그래서 타입이...","categories": ["Types and Programming Languages"],
+        "tags": [],
+        "url": "/types%20and%20programming%20languages/Ch3-Untyped_Arithmetic_Expressions/",
         "teaser": null
       }]
