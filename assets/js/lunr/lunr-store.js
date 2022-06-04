3df0e76d02c86d4a8ada9c119d20c45b93c961a3
oojahooo <oojahooo@gmail.com>
2022-06-04 18:51:45 +0900

    update

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": [],
         "url": "/types%20and%20programming%20languages/Ch0-Preface/",
         "teaser": null
+      },{
+        "title": "생애 첫 수업조교",
+        "excerpt":"기록물 1번 벌써 2년 가까이 지난 일이지만, 개인 홈페이지(겸 블로그)를 만들고 글을 쓰기로 마음먹은 뒤 아직도 서문만 쓰고 글을 쓰지 못하고 있는 나 자신을 위해, 또 아직까지도 꿈을 향하고 있는 내 원동력 벡터의 크기를 유지시켜주는 데 일조하고 있는 과거의 경험을 정리하기 위해 이 포스트를 남긴다. 또 기록하고 싶은 경험 또는...","categories": ["record"],
+        "tags": ["Teaching","Course"],
+        "url": "/record/KLUE/",
+        "teaser": null
       }]
